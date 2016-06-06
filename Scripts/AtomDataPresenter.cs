1c62cd0817f38380d62deec2c3de8ce6cdb24f40
aluszp <alicja.luszpinska@wp.pl>
2016-06-06 20:50:33 +0200

    ribbon- fully divided, first attempts in leap motion

---
+++ Scripts/AtomDataPresenter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AtomDataPresenter : MonoBehaviour {
+    private IEnumerator atomCoroutine;
+    public float delayInterval = .15f;
+    // Use this for initialization
+
+    void Awake () {
+        atomCoroutine = PresentData();
+    }
+
+    private IEnumerator PresentData()
+    {
+
+       
+        while (true)
+        {
+            print("no hejka");
+            yield return new WaitForSeconds(delayInterval);
+        }
+    }
+
+    public void StartPresenting()
+    {
+        StartCoroutine(atomCoroutine);
+    }
+
+    public void StopPresenting()
+    {
+        StopAllCoroutines();
+    }
+}
